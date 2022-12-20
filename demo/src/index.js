a4867fc002058aa3c39184b800731a7e2aac9c7e
Loïs Poujade <lois.poujade@tetras-libre.fr>
2022-12-20 14:54:57 +0100

    Fix linter + add demo manifests

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,3 @@
-
 import mirador from 'mirador/dist/es/src/index';
 import annotationPlugins from '../../src';
 import LocalStorageAdapter from '../../src/LocalStorageAdapter';
@@ -16,9 +15,16 @@ const config = {
     defaultSideBarPanel: 'annotations',
     sideBarOpenByDefault: true,
   },
-  windows: [{
-    loadedManifest: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
-  }],
+  catalog: [
+    { manifestId: 'https://dzkimgs.l.u-tokyo.ac.jp/videos/iiif_in_japan_2017/manifest.json' },
+    { manifestId: 'https://iiif.io/api/cookbook/recipe/0219-using-caption-file/manifest.json' },
+    { manifestId: 'https://preview.iiif.io/cookbook/master/recipe/0003-mvm-video/manifest.json' },
+    { manifestId: 'https://iiif.io/api/cookbook/recipe/0065-opera-multiple-canvases/manifest.json' },
+    { manifestId: 'https://iiif.io/api/cookbook/recipe/0064-opera-one-canvas/manifest.json' },
+    { manifestId: 'https://iiif.io/api/cookbook/recipe/0074-multiple-language-captions/manifest.json' },
+    { manifestId: 'https://iiif.harvardartmuseums.org/manifests/object/299843' },
+    { manifestId: 'https://iiif.io/api/cookbook/recipe/0002-mvm-audio/manifest.json' },
+  ]
 };
 
 mirador.viewer(config, [...annotationPlugins]);
