2a9621ab3d359144c4eaadf2522683b731c04c8f
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2024-03-12 09:38:37 +0100

    Fix saving SVG export of Konva. Wait all the annotations to be saved before to close companion window

--- src/annotationForm/TextCommentTemplate.js
+++ src/annotationForm/TextCommentTemplate.js
@@ -70,20 +70,24 @@ function TextCommentTemplate(
   };
 
   const saveFunction = () => {
-    canvases.forEach(async (canvas) => {
+    // Iterate over all canvases and save the annotation, then close the form
+
+    const promises = canvases.map(async (canvas) => {
       // Adapt target to the canvas
       // eslint-disable-next-line no-param-reassign
       console.log(annotation.maeData);
       annotationState.maeData.target.svg = await getSvg(windowId);
-     // annotationState.maeData.target.dataUrl = await getKonvaAsDataURL(windowId);
+      // annotationState.maeData.target.dataUrl = await getKonvaAsDataURL(windowId);
       annotationState.target = maeTargetToIiifTarget(annotationState.maeData.target, canvas.id);
       annotationState.maeData.target.drawingState = JSON.stringify(annotationState.maeData.target.drawingState);
       annotationState.maeData.target.svg = JSON.stringify(annotationState.maeData.target);
       console.log('annotationState', annotationState.target);
       // delete annotationState.maeData.target;
-      saveAnnotation(annotationState, canvas.id);
+      return saveAnnotation(annotationState, canvas.id);
     });
-    closeFormCompanionWindow();
+    Promise.all(promises).then(() => {
+      closeFormCompanionWindow();
+    });
   };
 
   useEffect(() => {
