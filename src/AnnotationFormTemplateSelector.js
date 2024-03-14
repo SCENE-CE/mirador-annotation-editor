2a5919f9bd45c07183d1bdc9d920a4d30a286f2c
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2024-03-14 15:13:21 +0100

    Fix information message displayed when using audio canva

--- src/AnnotationFormTemplateSelector.js
+++ src/AnnotationFormTemplateSelector.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
-import {Card, CardActionArea, CardContent, Grid} from '@mui/material';
+import {
+  Card, CardActionArea, CardContent, Grid,
+} from '@mui/material';
 import PropTypes from 'prop-types';
-import {mediaTypes, templateTypes} from './AnnotationFormUtils';
+import { mediaTypes, templateTypes } from './AnnotationFormUtils';
 /**
  * A component that renders a selection of annotation
  * form templates for different types of comments.
@@ -17,33 +19,34 @@ export default function AnnotationFormTemplateSelector({ setCommentingType, medi
   return (
     <CardContainer>
       {mediaType === mediaTypes.AUDIO ? (
-          <Grid container spacing={1} direction="column">
-            <Grid item>
-              <Typography> Mirador Annotation Editor Plugin does not support annotation on audio media.</Typography>
-            </Grid>
-
+        <Grid container spacing={1} direction="column">
+          <Grid item>
+            <Typography>
+              Mirador Annotation Editor Plugin does not support
+              annotation on audio media.
+            </Typography>
           </Grid>
+        </Grid>
       ) : (
-          templateTypes.map((t) => (
-                ( t.isCompatibleWithTemplate(mediaType) && (
-                    <Card>
-                      <CardActionArea id={t.id} onClick={() => setCommentType(t)}>
-                        <CardContent>
-                          <CardTypography variant="h6" component="div">
-                            {t.label}
-                            {t.icon}
-                            {t.isCompatibleWithTemplate(mediaType)}
-                          </CardTypography>
-                          <DescriptionCardTypography component="div" variant="body2">
-                            {t.description}
-                          </DescriptionCardTypography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                ))
-            ))
+        templateTypes.map((t) => (
+          (t.isCompatibleWithTemplate(mediaType) && (
+          <Card>
+            <CardActionArea id={t.id} onClick={() => setCommentType(t)}>
+              <CardContent>
+                <CardTypography variant="h6" component="div">
+                  {t.label}
+                  {t.icon}
+                  {t.isCompatibleWithTemplate(mediaType)}
+                </CardTypography>
+                <DescriptionCardTypography component="div" variant="body2">
+                  {t.description}
+                </DescriptionCardTypography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          ))
+        ))
       )}
-
     </CardContainer>
   );
 }
