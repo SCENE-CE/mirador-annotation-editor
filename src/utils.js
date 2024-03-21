abc65a79fd2c5bb3d6660014ec4e846a3d334dfd
Antoine <antoine.roy@tetras-libre.fr>
2024-03-21 15:03:12 +0100

    remove uselessCode

--- src/utils.js
+++ src/utils.js
@@ -8,18 +8,6 @@ export function mapChildren(layerThing) {
   return layerThing;
 }
 
-/** Pretty print a seconds count into HH:mm:ss */
-export function secondsToHMS(secs) {
-  const { hours, minutes, seconds } = secondsToHMSarray(secs);
-  /**
-   * Pads a number with a leading zero if it is less than 10.
-   * @param {number} n - The number to pad.
-   * @returns {string} The padded number as a string.
-   */
-  const pad = (n) => (n < 10 ? `0${n}` : n);
-  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
-}
-
 /** Split a second to { hours, minutes, seconds }  */
 export function secondsToHMSarray(secs) {
   const h = Math.floor(secs / 3600);
@@ -44,14 +32,4 @@ export const isValidUrl = (string) => {
   } catch (_) {
     return false;
   }
-};
-/**
- * Removes HTML tags from a given HTML string.
- *
- * @param {string} htmlString - The HTML string from which to remove tags.
- * @returns {string} The text content of the HTML string without any HTML tags.
- */
-export const removeHTMLTags = (htmlString) => {
-  const doc = new DOMParser().parseFromString(htmlString, 'text/html');
-  return doc.body.textContent || '';
 };
