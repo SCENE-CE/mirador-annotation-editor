2ceb332023a4101c20d35f1f04f740e499f2c17a
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2024-09-24 18:45:58 +0200

    Remove breaking manifest

--- demo/src/manifestsCatalog.js
+++ demo/src/manifestsCatalog.js
@@ -6,7 +6,6 @@ export const manifestsCatalog = [
   { manifestId: 'https://files.tetras-libre.fr/dev/Clock/manifestWithAnnotationTemplates_MAE.json' },
   { manifestId: 'https://files.tetras-libre.fr/dev/Clock/manifest.json' },
   { manifestId: 'https://files.tetras-libre.fr/dev/Hakanai/manifest/manifest.json' },
-  { manifestId: 'https://dzkimgs.l.u-tokyo.ac.jp/videos/iiif_in_japan_2017/manifest.json' },
   { manifestId: 'https://iiif.io/api/cookbook/recipe/0219-using-caption-file/manifest.json' },
   { manifestId: 'https://preview.iiif.io/cookbook/master/recipe/0003-mvm-video/manifest.json' },
   { manifestId: 'https://iiif.io/api/cookbook/recipe/0065-opera-multiple-canvases/manifest.json' },
