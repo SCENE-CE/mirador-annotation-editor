58a19bb44ffff772503ed53927d60c51c58d3ef8
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2024-09-26 23:53:43 +0200

    Clean ImageCommentTemplate.js

--- src/annotationForm/ImageCommentTemplate.js
+++ src/annotationForm/ImageCommentTemplate.js
@@ -256,13 +256,6 @@ export default function ImageCommentTemplate(
         spatialTarget={false}
         debugMode={debugMode}
       />
-      {/* <Grid item> */}
-      {/*   <Debug */}
-      {/*     scale={scale} */}
-      {/*     drawingState={drawingState} */}
-      {/*   /> */}
-      {/* </Grid> */}
-
       <Grid item>
         <AnnotationFormFooter
           closeFormCompanionWindow={closeFormCompanionWindow}
