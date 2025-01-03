d715490161242a82ffc0de9b1831e31e96e45222
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-01-03 17:51:12 +0100

    Reorganize playerReferences.js

--- src/playerReferences.js
+++ src/playerReferences.js
@@ -4,115 +4,155 @@ import { MEDIA_TYPES } from './annotationForm/AnnotationFormUtils';
 
 // TODO All the code related to the video player must be moved in MAEV plugin
 export const playerReferences = (function () {
-  let _canvases;
-  let _media;
-  let _mediaType;
-  let _overlay;
-  let _actions;
-  let _audio;
+  let canvases;
+  let media;
+  let mediaType;
+  let overlay;
+  let actions;
+  let audio;
 
   return {
-    getScale() {
-      return this.getDisplayedImageWidth() / this.getWidth();
+    /** ***********************************************************
+     * Global stuff
+     *********************************************************** */
+
+    /** ***********************
+     * Get media type of visible canvas
+     * @param state
+     * @param windowId
+     * @returns {string}
+     */
+    checkMediaType(state, windowId) {
+      const audioResources = getVisibleCanvasAudioResources(state, { windowId }) || [];
+      const videoResources = getVisibleCanvasVideoResources(state, { windowId }) || [];
+
+      if (videoResources.length > 0) {
+        return MEDIA_TYPES.VIDEO;
+      }
+      if (audioResources.length > 0) {
+        return MEDIA_TYPES.AUDIO;
+      }
+
+      return MEDIA_TYPES.IMAGE;
     },
+    getMediaType() {
+      return mediaType;
+    },
+    /** *******************
+     * Get all canvases
+     * @returns {*}
+     */
+    getCanvases() {
+      return canvases;
+    },
+    getAudioElement() {
+      return document.querySelector('audio');
+    },
+
+    /** ***********************************************************
+     * Spatial stuff
+     *********************************************************** */
     getCanvasHeight() {
-      return _overlay.canvasHeight;
+      return overlay.canvasHeight;
     },
     getCanvasWidth() {
-      return _overlay.canvasWidth;
-    },
-    getCanvases() {
-      return _canvases;
+      return overlay.canvasWidth;
     },
     getContainer() {
-      if (_mediaType === MEDIA_TYPES.IMAGE) {
-        return _media.current.container;
+      if (mediaType === MEDIA_TYPES.IMAGE) {
+        return media.current.container;
       }
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        return _media.ref.current.parentElement;
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        return media.ref.current.parentElement;
       }
       return null;
     },
     getContainerHeight() {
-      return _overlay.containerHeight;
+      return overlay.containerHeight;
     },
     getContainerWidth() {
-      return _overlay.containerWidth;
-    },
-    getHeight() {
-      if (_mediaType === MEDIA_TYPES.IMAGE) {
-        return _canvases[0].__jsonld.height;
-      }
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        // TODO not perfect becasue we use the canvas size and not the video size
-        return _media.player.props.iiifVideoInfos.getHeight();
-      }
-      console.error('Unknown media type');
-      return undefined;
-    },
-    getMediaDuration() {
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        return _media.props.canvas.__jsonld.duration;
-      }
-      if (_mediaType === MEDIA_TYPES.AUDIO) {
-        if (_audio) {
-          return _audio[0].__jsonld.duration;
-        }
-        console.error('Something is wrong about audio');
-      }
-    },
-
-    getOverlay() {
-      return _overlay;
-    },
-    getWidth() {
-      if (_mediaType === MEDIA_TYPES.IMAGE) {
-        return _canvases[0].__jsonld.width;
-      }
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        // TODO not perfect becasue we use the canvas size and not the video size
-        return _media.player.props.iiifVideoInfos.getWidth();
-      }
-      return undefined;
-    },
-    getDisplayedImageWidth() {
-      if (_mediaType === MEDIA_TYPES.IMAGE) {
-        const viewer = _media.current;
-        if (viewer && viewer.world.getItemCount() > 0) {
-          const percentageWidth = _canvases[0].__jsonld.width * viewer.viewport.getZoom();
-          const containerWidth = viewer.container.clientWidth;
-          const actualWidthInPixels = Math.round(containerWidth * percentageWidth);
-          return actualWidthInPixels;
-        }
-      }
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        // TODO: Implement displayed width for video
-        // From video file return Math.round(_media.video.getBoundingClientRect().width);
-        // return _media.video.getSize().width;
-        return _overlay.containerWidth;
-      }
-
-      return undefined;
+      return overlay.containerWidth;
     },
     getDisplayedImageHeight() {
-      if (_mediaType === MEDIA_TYPES.IMAGE) {
-        const viewer = _media.current;
+      // TODO This function can cause problem in multiple window context
+      if (mediaType === MEDIA_TYPES.IMAGE) {
+        const viewer = media.current;
         if (viewer) {
-          const percentageHeight = _canvases[0].__jsonld.height * viewer.viewport.getZoom();
+          const percentageHeight = canvases[0].__jsonld.height * viewer.viewport.getZoom();
           const containerWidth = viewer.container.clientWidth;
           const actualHeightInPixels = Math.round(containerWidth * percentageHeight);
           return actualHeightInPixels;
         }
       }
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        return _overlay.containerHeight;
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        return overlay.containerHeight;
+      }
+      return undefined;
+    },
+    getDisplayedImageWidth() {
+      // TODO This function can cause problem in multiple window context
+      if (mediaType === MEDIA_TYPES.IMAGE) {
+        const viewer = media.current;
+        if (viewer && viewer.world.getItemCount() > 0) {
+          const percentageWidth = canvases[0].__jsonld.width * viewer.viewport.getZoom();
+          const containerWidth = viewer.container.clientWidth;
+          const actualWidthInPixels = Math.round(containerWidth * percentageWidth);
+          return actualWidthInPixels;
+        }
+      }
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        // TODO: Implement displayed width for video
+        // From video file return Math.round(_media.video.getBoundingClientRect().width);
+        // return _media.video.getSize().width;
+        return overlay.containerWidth;
+      }
+
+      return undefined;
+    },
+    getHeight() {
+      // TODO This function can cause problem in multiple window context
+      if (mediaType === MEDIA_TYPES.IMAGE) {
+        return canvases[0].__jsonld.height;
+      }
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        // TODO not perfect becasue we use the canvas size and not the video size
+        return media.player.props.iiifVideoInfos.getHeight();
+      }
+      console.error('Unknown media type');
+      return undefined;
+    },
+
+    getScale() {
+      return this.getDisplayedImageWidth() / this.getWidth();
+    },
+    getWidth() {
+      if (mediaType === MEDIA_TYPES.IMAGE) {
+        return canvases[0].__jsonld.width;
+      }
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        // TODO not perfect becasue we use the canvas size and not the video size
+        return media.player.props.iiifVideoInfos.getWidth();
+      }
+      return undefined;
+    },
+
+    getZoom() {
+      if (mediaType === MEDIA_TYPES.IMAGE) {
+        const currentZoom = media.current.viewport.getZoom();
+        const maxZoom = media.current.viewport.getMaxZoom();
+        // console.log("Max Zoom", maxZoom);
+        let zoom = currentZoom / maxZoom;
+        zoom = Math.round(zoom * 100) / 100;
+        return zoom;
+      }
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        return overlay.containerWidth / this.getWidth();
       }
       return undefined;
     },
     getImagePosition() {
-      // TODO: Index off the IIIF canvas instead of the first image
-      if (_mediaType === MEDIA_TYPES.IMAGE) {
-        const viewer = _media.current;
+      if (mediaType === MEDIA_TYPES.IMAGE) {
+        const viewer = media.current;
         if (viewer) {
           // Assuming one image in OpenSeadragon for now
           const tiledImage = viewer.world.getItemAt(0);
@@ -128,66 +168,72 @@ export const playerReferences = (function () {
           return position;
         }
       }
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
+      if (mediaType === MEDIA_TYPES.VIDEO) {
         const position = {
           x: 0,
           y: 0,
-        }; // TODO need to implement position on vertical video
+        };
         return position;
       }
       return undefined;
     },
-    getZoom() {
-      if (_mediaType === MEDIA_TYPES.IMAGE) {
-        const currentZoom = _media.current.viewport.getZoom();
-        const maxZoom = _media.current.viewport.getMaxZoom();
-        // console.log("Max Zoom", maxZoom);
-        let zoom = currentZoom / maxZoom;
-        zoom = Math.round(zoom * 100) / 100;
-        return zoom;
+    /** ***********************************************************
+     * Time stuff
+     *********************************************************** */
+    getCurrentTime() {
+      if (mediaType !== MEDIA_TYPES.IMAGE) {
+        return media.props.currentTime;
       }
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        return _overlay.containerWidth / this.getWidth();
-      }
-      return undefined;
+      return null;
     },
-    getMediaType() {
-      return _mediaType;
-    },
-    checkMediaType(state, windowId) {
-      const audioResources = getVisibleCanvasAudioResources(state, { windowId }) || [];
-      const videoResources = getVisibleCanvasVideoResources(state, { windowId }) || [];
-
-      if (videoResources.length > 0) {
-        return MEDIA_TYPES.VIDEO;
+    getMediaDuration() {
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        return media.props.canvas.__jsonld.duration;
       }
-      if (audioResources.length > 0) {
-        return MEDIA_TYPES.AUDIO;
+      if (mediaType === MEDIA_TYPES.AUDIO) {
+        if (audio) {
+          return audio[0].__jsonld.duration;
+        }
+        console.error('Something is wrong about audio');
       }
-
-      return MEDIA_TYPES.IMAGE;
     },
-    init(state, windowId, playerRef, actions) {
-      _actions = actions;
-      _media = playerRef.get(windowId);
-      _mediaType = this.checkMediaType(state, windowId);
-      _canvases = getVisibleCanvases(state, { windowId });
+    setCurrentTime(windowId, ...args) {
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        return actions.setWindowCurrentTime(windowId, ...args);
+      }
+      console.error('Cannot set current time for image');
+    },
 
-      if (_media) {
-        switch (_mediaType) {
+    setSeekTo(windowId, ...args) {
+      if (mediaType === MEDIA_TYPES.VIDEO) {
+        return actions.setWindowSeekTo(windowId, ...args);
+      }
+      console.error('Cannot seek time for image');
+    },
+    /** ***********************************************************
+     * Init stuff
+     *********************************************************** */
+    init(state, windowId, playerRef, miradorActions) {
+      actions = miradorActions;
+      media = playerRef.get(windowId);
+      mediaType = this.checkMediaType(state, windowId);
+      canvases = getVisibleCanvases(state, { windowId });
+
+      if (media) {
+        switch (mediaType) {
           case MEDIA_TYPES.IMAGE:
-            _overlay = {
-              canvasHeight: _media.current.canvas.clientHeight,
-              canvasWidth: _media.current.canvas.clientWidth,
-              containerHeight: _media.current.canvas.clientHeight,
-              containerWidth: _media.current.canvas.clientWidth,
+            overlay = {
+              canvasHeight: media.current.canvas.clientHeight,
+              canvasWidth: media.current.canvas.clientWidth,
+              containerHeight: media.current.canvas.clientHeight,
+              containerWidth: media.current.canvas.clientWidth,
             };
             break;
           case MEDIA_TYPES.VIDEO:
-            _overlay = _media.canvasOverlay;
+            overlay = media.canvasOverlay;
             break;
           case MEDIA_TYPES.AUDIO:
-            _audio = getVisibleCanvasAudioResources(state, { windowId });
+            audio = getVisibleCanvasAudioResources(state, { windowId });
             break;
           default:
             console.error('Unknown media type');
@@ -195,35 +241,11 @@ export const playerReferences = (function () {
         }
       }
     },
-
     isInitialized() {
       // TODO this part must be clarified
       // Its not exactly initialisation but if the player is available for the media type
-      return _media && (_media.current || _media.video) && (_mediaType !== MEDIA_TYPES.UNKNOWN && _mediaType !== MEDIA_TYPES.AUDIO);
-    },
-
-    // TODO internalize actions
-    setCurrentTime(windowId, ...args) {
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        return _actions.setWindowCurrentTime(windowId, ...args);
-      }
-      console.error('Cannot set current time for image');
-    },
-    getCurrentTime() {
-      if (_mediaType !== mediaTypes.IMAGE) {
-        return _media.props.currentTime;
-      }
-      return null;
-    },
-    setSeekTo(windowId, ...args) {
-      if (_mediaType === MEDIA_TYPES.VIDEO) {
-        return _actions.setWindowSeekTo(windowId, ...args);
-      }
-      console.error('Cannot seek time for image');
-    },
-
-    getAudioElement() {
-      return document.querySelector('audio');
+      return media && (media.current || media.video)
+        && (mediaType !== MEDIA_TYPES.UNKNOWN && mediaType !== MEDIA_TYPES.AUDIO);
     },
   };
 }());
