e4213541a584410cad0b6fa251b9e2be86b8a3d9
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-01-07 18:30:02 +0100

    Convert externalStorageAnnotationPlugin.js to functionnal component

--- src/plugins/externalStorageAnnotationPlugin.js
+++ src/plugins/externalStorageAnnotationPlugin.js
@@ -1,75 +1,64 @@
-import React, { Component } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { getVisibleCanvases } from 'mirador/dist/es/src/state/selectors/canvases';
-import isEqual from 'lodash/isEqual';
 import * as actions from 'mirador/dist/es/src/state/actions';
 import LocalStorageAdapter from '../annotationAdapter/LocalStorageAdapter';
 import AnnototAdapter from '../annotationAdapter/AnnototAdapter';
 import { AnnotationAdapter } from '../annotationAdapter/AnnotationAdapterUtils';
 
-// TODO: Change this classComponent into functional component
-/** */
-class ExternalStorageAnnotation extends Component {
-  /** */
-  constructor(props) {
-    super(props);
-    this.retrieveAnnotations = this.retrieveAnnotations.bind(this);
-  }
-
-  /** */
-  componentDidMount() {
-    const { canvases } = this.props;
-    this.retrieveAnnotations(canvases);
-  }
-
-  /** */
-  componentDidUpdate(prevProps) {
-    const { canvases } = this.props;
-    const currentCanvasIds = canvases.map((canvas) => canvas.id);
-    const prevCanvasIds = prevProps.canvases.map((canvas) => canvas.id);
-    if (!isEqual(currentCanvasIds, prevCanvasIds)) {
-      this.retrieveAnnotations(canvases);
-    }
-  }
-
-  /** */
-  retrieveAnnotations(canvases) {
-    const { config, receiveAnnotation } = this.props;
-
-    canvases.forEach((canvas) => {
-      // TODO Actually when exporting workspace, adapter is not saved.
-      // So, we can manually add : adapter : 'localStorage' or 'annotot'
-      if (typeof config.annotation.adapter === 'string' && config.annotation.adapter === AnnotationAdapter.LOCAL_STORAGE) {
-        config.annotation.adapter = (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`);
+/** Functional component version of ExternalStorageAnnotation */
+function ExternalStorageAnnotation({
+  canvases,
+  config,
+  receiveAnnotation,
+  PluginComponents,
+  TargetComponent,
+  targetProps,
+}) {
+  const retrieveAnnotations = useCallback((currentCanvases) => {
+    currentCanvases.forEach((canvas) => {
+      if (
+        typeof config.annotation.adapter === 'string'
+        && config.annotation.adapter === AnnotationAdapter.LOCAL_STORAGE
+      ) {
+        config.annotation.adapter = (canvasId) =>
+          new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`);
       }
-      if(typeof config.annotation.adapter === 'string' && config.annotation.adapter  === AnnotationAdapter.ANNOTOT) {
+      if (
+        typeof config.annotation.adapter === 'string'
+        && config.annotation.adapter === AnnotationAdapter.ANNOTOT
+      ) {
         const endpointUrl = 'http://127.0.0.1:3000/annotations';
         config.annotation.adapter = (canvasId) => new AnnototAdapter(canvasId, endpointUrl);
       }
       if (!config.annotation.adapter) {
-        // Default adapter set to LocalStorageAdapter
-        config.annotation.adapter = (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`);
+        config.annotation.adapter = (canvasId) =>
+          new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`);
       }
       const storageAdapter = config.annotation.adapter(canvas.id);
 
       storageAdapter.all().then((annoPage) => {
         if (annoPage) {
-          receiveAnnotation(canvas.id, storageAdapter.annotationPageId, annoPage);
+          receiveAnnotation(
+            canvas.id,
+            storageAdapter.annotationPageId,
+            annoPage,
+          );
         }
       });
     });
-  }
+  }, [config, receiveAnnotation]);
 
-  /** */
-  render() {
-    const { PluginComponents, TargetComponent, targetProps } = this.props;
-    return (
-      <TargetComponent
-        {...targetProps} // eslint-disable-line react/jsx-props-no-spreading
-        PluginComponents={PluginComponents}
-      />
-    );
-  }
+  useEffect(() => {
+    retrieveAnnotations(canvases);
+  }, [canvases, retrieveAnnotations]);
+
+  return (
+    <TargetComponent
+      {...targetProps} // eslint-disable-line react/jsx-props-no-spreading
+      PluginComponents={PluginComponents}
+    />
+  );
 }
 
 ExternalStorageAnnotation.propTypes = {
