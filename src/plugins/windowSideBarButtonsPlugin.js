058d0ee0efcb5906cc7f8733e609c381141a46b9
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-01-07 18:34:40 +0100

    Convert windowSideBarButtonsPlugin.js to functionnal component

--- src/plugins/windowSideBarButtonsPlugin.js
+++ src/plugins/windowSideBarButtonsPlugin.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-// TODO: Change this classComponent into functional component
+
 /**
  * A wrapper plugin that sets hasAnyAnnotations to true so that the annotation
  * companion window button is present
  */
-class WindowSideBarButtonWrapper extends Component {
-  /** */
-  render() {
-    const { PluginComponents, TargetComponent, targetProps } = this.props;
-    targetProps.hasAnyAnnotations = true;
-    return (
-      <TargetComponent
-        {...targetProps} // eslint-disable-line react/jsx-props-no-spreading
-        PluginComponents={PluginComponents}
-      />
-    );
-  }
-}
+const WindowSideBarButtonWrapper = ({ PluginComponents, TargetComponent, targetProps }) => {
+  // eslint-disable-next-line no-param-reassign
+  targetProps.hasAnyAnnotations = true;
+
+  return (
+    <TargetComponent
+      {...targetProps} // eslint-disable-line react/jsx-props-no-spreading
+      PluginComponents={PluginComponents}
+    />
+  );
+};
 
 WindowSideBarButtonWrapper.propTypes = {
   PluginComponents: PropTypes.array, // eslint-disable-line react/forbid-prop-types
