de4c1ede705e293b5668c8bc481d0d0b14c0252b
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-01-11 11:43:00 +0100

    Simplify IIIF Utils, maeTarget to IIIFAnnot

--- src/IIIFUtils.js
+++ src/IIIFUtils.js
@@ -6,6 +6,23 @@ import { playerReferences } from './playerReferences';
 import { TEMPLATE } from './annotationForm/AnnotationFormUtils';
 
 /**
+ * Check if annotation is exportable to image in case of Konva annotation
+ * @param maeData
+ * @returns {boolean}
+ */
+function isAnnotationExportableToImage(maeData) {
+  if (maeData.templateType === TEMPLATE.KONVA_TYPE) {
+    if (maeData.target.drawingState.shapes.length > 1) {
+      return true;
+    }
+    if (maeData.target.drawingState.shapes.length === 1 && maeData.target.drawingState.shapes[0].type !== 'rectangle') {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
  * Convert annotation state to be saved. Function change the annotationState object
  * @param annotationState
  * @param canvas
@@ -41,7 +58,7 @@ export const convertAnnotationStateToBeSaved = async (
     }
   }
 
-  if (annotationStateForSaving.maeData.templateType == TEMPLATE.KONVA_TYPE) {
+  if (isAnnotationExportableToImage(annotationStateForSaving.maeData)) {
     annotationStateForSaving.body.id = await getKonvaAsDataURL(windowId);
     annotationStateForSaving.body.format = 'image/jpg';
     annotationStateForSaving.type = 'Annotation';
@@ -49,6 +66,7 @@ export const convertAnnotationStateToBeSaved = async (
 
   if (annotationStateForSaving.maeData.templateType == TEMPLATE.IMAGE_TYPE) {
     if (annotationStateForSaving.maeData.target.drawingState.shapes.length == 1) {
+      // eslint-disable-next-line max-len
       annotationStateForSaving.body.id = annotationStateForSaving.maeData.target.drawingState.shapes[0].url;
       annotationStateForSaving.type = 'Annotation';
     }
@@ -74,7 +92,6 @@ export const convertAnnotationStateToBeSaved = async (
 /** Transform maetarget to IIIF compatible data * */
 export const maeTargetToIiifTarget = (maeTarget, canvasId) => {
   if (maeTarget.templateType === TEMPLATE.IIIF_TYPE) {
-    // CHeck that one
     return maeTarget;
   }
 
@@ -96,24 +113,16 @@ export const maeTargetToIiifTarget = (maeTarget, canvasId) => {
     };
   }
 
-  if (maeTarget.drawingState.shapes.length == 0) {
-    console.info('Implement target as string on fullSizeCanvas');
-    return `${canvasId}#` + `xywh=${maeTarget.fullCanvaXYWH}&t=${maeTarget.tstart},${maeTarget.tend}`;
-  }
-
   if (maeTarget.drawingState.shapes.length === 1 && maeTarget.drawingState.shapes[0].type === 'rectangle' || maeTarget.drawingState.shapes[0].type == 'image') {
     let {
       // eslint-disable-next-line prefer-const
       x, y, width, height,
     } = maeTarget.drawingState.shapes[0];
-   /*  x = Math.floor(x * maeTarget.scale * scaleX);
-    y = Math.floor(y * maeTarget.scale * scaleY);
-    width = Math.floor(width * maeTarget.scale * scaleX);
-    height = Math.floor(height * maeTarget.scale * scaleY); */
     console.info('Implement target as string with one shape (reactangle or image)');
     // Image have not tstart and tend
     return `${canvasId}#${maeTarget.tend ? `xywh=${x},${y},${width},${height}&t=${maeTarget.tstart},${maeTarget.tend}` : `xywh=${x},${y},${width},${height}`}`;
   }
 
+  console.info('Implement target as string on fullSizeCanvas');
   return `${canvasId}#${maeTarget.tend ? `xywh=${maeTarget.fullCanvaXYWH}&t=${maeTarget.tstart},${maeTarget.tend}` : `xywh=${maeTarget.fullCanvaXYWH}`}`;
 };
